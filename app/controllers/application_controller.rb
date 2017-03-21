86b3fa24ae5dea41a9158a74f71277d49cfd87e5
DBombay <d.bombay@live.com>
2017-03-21 11:05:40 -0400

    adds devise links to nav bar. Updates seed file with admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitzer.permit(:sign_up, keys: [:first_name, :last_name, :age, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :age, :gender])
   end
 end
