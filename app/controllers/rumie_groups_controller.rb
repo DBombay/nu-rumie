419ee4f5b0cd9ed7b09a6f5cae9a47c458b6bb0f
DBombay <d.bombay@live.com>
2017-03-26 18:05:08 -0400

    adds some rumie group views and updates controller. Updates additional devise views.

--- app/controllers/rumie_groups_controller.rb
+++ app/controllers/rumie_groups_controller.rb
@@ -5,6 +5,18 @@ class RumieGroupsController < ApplicationController
 
   def create
     @rumie = RumieGroup.new
+
+    if @rumie.save
+      flash[:notice] = "you've successfully formed a rumie group!"
+      redirect to @rumie
+    else
+      flash[:notice] = @bar.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    @rumies = RumieGroup.all
   end
 
 end
