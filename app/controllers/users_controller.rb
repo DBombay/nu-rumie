6f06af26345804e45ef033f43931a87287a14606
DBombay <d.bombay@live.com>
2017-03-28 16:03:35 -0400

    allows users to self-add and self-remove from a group. Allows group creators to remove users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show, :update]
 
   def index
     @users = User.all
@@ -7,5 +7,25 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.rumie_group
+      @rumie = RumieGroup.find(@user.rumie_group)
+      @user.rumie_group = nil
+      if @user.save
+        flash[:notice] = "#{@user.first_name.downcase} removed successfully from
+        #{@rumie.group_name.downcase}"
+      else
+        flash[:notice] = @rumie.errors.full_messages
+      end
+      redirect_to root_path
+    else
+      @rumie = RumieGroup.find(params[:group_id])
+      @rumie.users << @user
+      flash[:notice] = "you've succesfully joined #{@rumie.group_name}!"
+      redirect_to @rumie
+    end
   end
 end
